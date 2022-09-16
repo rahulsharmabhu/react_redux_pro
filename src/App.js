534576a955f976fd4304f54b6189995c51ca3dfe
Rahul Sharma <rahulsharmabhumca16@gmail.com>
2022-09-16 16:33:10 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Headers from './containers/Headers';
+import { BrowserRouter, Switch, Route} from "react-router-dom";
+import ProductListing from './containers/ProductListing';
+import ProductDetails from './containers/ProductDetails';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Headers/>
+      <Switch>
+        <Route exact path="/" component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Route> 404 Not Found ! </Route>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/containers/Headers.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Headers = () => {
+  return (
+    <div className='ui fixed menu'>
+        <div className='ui container center'>
+          <h2>Fake Store</h2>
+        </div>
+    </div>
+  )
+}
+
+export default Headers
